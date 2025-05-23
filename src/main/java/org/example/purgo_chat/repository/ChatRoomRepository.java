1df115a5cb7fcf99fd319bb040391799c7853c04
Sanghyuk <pyosang0919@gmail.com>
2025-05-23 17:50:59 +0900

    aws_chat
    aws_chat

--- src/main/java/org/example/purgo_chat/repository/ChatRoomRepository.java
+++
@@ -1,8 +0,0 @@
-package org.example.purgo_chat.repository;
-
-import org.example.purgo_chat.entity.ChatRoom;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface ChatRoomRepository extends JpaRepository<ChatRoom, Integer> {
-}

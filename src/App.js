dcffc8deaa68b38983dbe3ef3f38007e76c5e478
Sanghyuk <pyosang0919@gmail.com>
2025-05-28 10:34:04 +0900

    닉네임 페이지 로고 추가
    닉네임 페이지 로고 추가

--- src/App.js
+++ src/App.js
@@ -18,7 +18,26 @@ const App = () => {
         <NicknameProvider>
             <Router>
                 <Routes>
-                    <Route path="/" element={<NicknamePage />} />
+                    <Route
+                        path="/"
+                        element={
+                            <div className="bg-[#C2DFFF] min-h-screen relative flex justify-center items-center">
+                                {/* PURGO 링크 */}
+                                <a
+                                    href="http://purgo.kro.kr"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="absolute top-4 left-4 px-4 py-2 bg-white text-blue-600 font-bold rounded-full shadow hover:bg-blue-100 transition cursor-pointer z-50"
+                                >
+                                    PURGO
+                                </a>
+
+                                <IPhoneMockup>
+                                    <NicknamePage />
+                                </IPhoneMockup>
+                            </div>
+                        }
+                    />
                     <Route
                         path="/chat"
                         element={
--- src/pages/NicknamePage.js
+++ src/pages/NicknamePage.js
@@ -1,17 +1,12 @@
 import React from "react";
 import NicknameForm from "../components/NicknameForm";
-import IPhoneMockup from "../Mockup/IPhoneMockup";
 
 const NicknamePage = () => {
     return (
-        <div className="flex justify-center items-center h-screen bg-[#C2DFFF] overflow-hidden">
-            <IPhoneMockup>
-                <div className="flex flex-col h-full w-full max-w-sm mx-auto bg-white pt-6 px-6 justify-start pb-6">
-                    <div className="mt-[54%]">
-                        <NicknameForm />
-                    </div>
-                </div>
-            </IPhoneMockup>
+        <div className="flex flex-col h-full w-full max-w-sm mx-auto bg-white pt-6 px-6 justify-start pb-6">
+            <div className="mt-[54%]">
+                <NicknameForm />
+            </div>
         </div>
     );
 };
